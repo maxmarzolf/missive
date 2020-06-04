c959dec730fbcef9cc5c3279f8cca60aa1775b06
Brett-BI <b.m.burleyinners@gmail.com>
2020-06-03 23:13:54 -0400

    Parser attributes for elements of the request are instantiated when the Parser class is instantiated.

--- app/sms/Parser.py
+++ app/sms/Parser.py
@@ -18,50 +18,53 @@ class ParserInterface(ABC):
 class AddParser(ParserInterface):
     def __init__(self, msg_body):
         self.msg_body = msg_body
-        self.times = []
-        self.reminder = []
-        self.receivers = []
+        self.times = self.parseTimes()
+        self.reminder = self.parseReminder()
+        self.receivers = self.parseReceivers()
         self.elements = []
 
     def getElements(self):
-        for t in self.parseTimes():
+        for t in self.times:
             self.elements.append(t)
 
-        for m in self.parseReminder():
+        for m in self.reminder:
             self.elements.append(m)
 
-        for r in self.parseReceivers():
+        for r in self.receivers:
             self.elements.append(r)
 
         return self.elements
 
     def parseTimes(self):
+        times = []
         split_times = re.findall(r'(@\d+:\d+\s*\w*)', self.msg_body)
 
         for t in split_times:
-            self.times.append(t.strip())
+            times.append(t.strip())
 
-        return self.times
+        return times
     
     def parseReceivers(self):
+        receivers = []
         split_receivers = re.findall(r'(#\d+)', self.msg_body)
 
         for r in split_receivers:
-            self.receivers.append(r.strip())
+            receivers.append(r.strip())
         
-        return self.receivers
+        return receivers
 
     def parseReminder(self):
+        reminders = []
         split_reminders = re.findall(r'(\"[^\"]*\")', self.msg_body)
 
         for r in split_reminders:
-            self.reminder.append(r.strip())
+            reminders.append(r.strip())
 
-        return self.reminder
+        return reminders
 
     def __repr__(self):
         return self.msg_body
 
 ap = AddParser('+ @12:00 @13:00 "Get coffee" "get beans" #1234567899')
 print(ap.msg_body)
-print(ap.getElements())
+print(ap.elements)
